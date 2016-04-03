0a4f6578cf9e5ac0978a364f5a0e6d6ca771ed91
Gert De Geyter <gertdegeyter@gmail.com>
2016-04-03 15:18:17 +0200

    end of video 15

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import SignUpForm
+from .forms import ContactForm, SignUpForm
 
 # Create your views here.
 def home(request):
@@ -37,3 +37,31 @@ def home(request):
 
 
     return render(request, "home.html", context)
+
+def contact(request):
+
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+        for key, value in  form.cleaned_data.items():
+            print(key,value)
+        # email = form.cleaned_data.get('email')
+        # message = form.cleaned_data.get('message')
+        # full_name = form.cleaned_data.get('full_name')
+        # print(email, message, full_name)
+
+
+    context = {
+        "form": form,
+    }
+
+
+    return render(request, "forms.html", context)
+
+
+
+
+
+
+
+
