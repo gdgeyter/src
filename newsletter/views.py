68a00492691218e111b47d8a15c36ee1a47e0563
Gert De Geyter <gertdegeyter@gmail.com>
2016-04-05 08:31:43 +0200

    end of video 24 (bootstrap grid)

--- newsletter/views.py
+++ newsletter/views.py
@@ -38,7 +38,7 @@ def home(request):
         print(instance.timestamp)
 
 
-    return render(request, "example_fluid.html", context)
+    return render(request, "home.html", context)
 
 def contact(request):
 
